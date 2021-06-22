422cc96ccd808842d12f0699d9904a2bf6f8aec7
sivamedikonda1234 <86262280+sivamedikonda1234@users.noreply.github.com>
2021-06-22 13:01:14 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
-resource "google_compute_subnetwork" "subenet-mhr-hr" {
+resource "google_compute_subnetwork" "subenet-siva-hr" {
   name          = var.subnetname
   ip_cidr_range = var.cidr
   region        = var.region
-  network       = google_compute_network.vpc-mhr-hr.id
+  network       = google_compute_network.vpc-siva-hr.id
 }
 
-resource "google_compute_network" "vpc-mhr-hr" {
+resource "google_compute_network" "vpc-siva-hr" {
   name                    = var.vpcname
   auto_create_subnetworks = false
 }
