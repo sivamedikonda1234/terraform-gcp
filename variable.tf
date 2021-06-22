b48eed7931cb85bd016fbacd78a7bc02da596340
sivamedikonda1234 <86262280+sivamedikonda1234@users.noreply.github.com>
2021-06-22 12:01:18 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "vpcname" {  }
+
+variable "subnetname" { }
+
+variable "zone" {
+
+default = "us-central1-c"
+}
+
+variable "region" { }
+variable "instancename" { }
+
+variable "machinetype"   { }
+
+variable "cidr" { }
